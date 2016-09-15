8fa36b6d57a086e48958b5ebc39bae29295524a6
and2long <and2long@gmail.com>
2016-09-15 12:26:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/and2long/recyclerviewaddheader/BaseRecyclerViewAdapter.java
@@ -0,0 +1,101 @@
+package com.example.and2long.recyclerviewaddheader;
+
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by and2long on 16-9-15.
+ */
+public class BaseRecyclerViewAdapter extends RecyclerView.Adapter {
+
+    public static final int TYPE_HEADER = 0;
+    public static final int TYPE_NORMAL = 1;
+    private View mHeaderView;
+    private ArrayList<String> mDatas;
+
+    public void addData(ArrayList<String> data) {
+        mDatas = data;
+    }
+
+    public void setHeaderView(View mHeaderView) {
+        this.mHeaderView = mHeaderView;
+    }
+
+    public View getHeaderView() {
+        return mHeaderView;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        RecyclerView.LayoutManager layoutManager = recyclerView.getLayoutManager();
+        if (layoutManager instanceof GridLayoutManager) {
+            final GridLayoutManager gridManager = (GridLayoutManager) layoutManager;
+            gridManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    return getItemViewType(position) == TYPE_HEADER ? gridManager.getSpanCount() : 1;
+                }
+            });
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHeaderView == null) {
+            return TYPE_NORMAL;
+        }
+        if (position == 0) {
+            return TYPE_HEADER;
+        }
+        return TYPE_NORMAL;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new Holder(mHeaderView);
+        }
+        View layout = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new Holder(layout);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_HEADER) {
+            return;
+        }
+        ((Holder) holder).text.setText(mDatas.get(getRealPosition(holder)));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mHeaderView == null ? mDatas.size() : mDatas.size() + 1;
+    }
+
+    public int getRealPosition(RecyclerView.ViewHolder holder) {
+        int position = holder.getLayoutPosition();
+        return mHeaderView == null ? position : position - 1;
+    }
+
+    class Holder extends RecyclerView.ViewHolder {
+
+        TextView text;
+
+        public Holder(View itemView) {
+            super(itemView);
+            if (itemView == mHeaderView) {
+                return;
+            }
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+
+}
+
---
+++ app/src/main/java/com/example/and2long/recyclerviewaddheader/MainActivity.java
@@ -0,0 +1,83 @@
+package com.example.and2long.recyclerviewaddheader;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private RecyclerView mRecyclerView;
+    private BaseRecyclerViewAdapter adapter;
+    private LinearLayoutManager linearLayoutManager;
+    private LinearLayoutManager gridLayoutManager;
+    private ArrayList<String> data1;
+    private ArrayList<String> data2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
+        linearLayoutManager = new LinearLayoutManager(this);
+        gridLayoutManager = new GridLayoutManager(this, 2);
+
+        View header = View.inflate(this, R.layout.header, null);
+        Button btnSingle = (Button) header.findViewById(R.id.btn_single);
+        Button btnDouble = (Button) header.findViewById(R.id.btn_double);
+        Button btnData1 = (Button) header.findViewById(R.id.btn_data1);
+        Button btnData2 = (Button) header.findViewById(R.id.btn_data2);
+        btnSingle.setOnClickListener(this);
+        btnDouble.setOnClickListener(this);
+        btnData1.setOnClickListener(this);
+        btnData2.setOnClickListener(this);
+
+
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        adapter = new BaseRecyclerViewAdapter();
+        adapter.setHeaderView(header);
+        adapter.addData(data1);
+        mRecyclerView.setAdapter(adapter);
+    }
+
+    private void initData() {
+        data1 = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data1.add(i + "");
+        }
+        data2 = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            data2.add(i + "...");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_single:
+                mRecyclerView.setLayoutManager(linearLayoutManager);
+                break;
+            case R.id.btn_double:
+                mRecyclerView.setLayoutManager(gridLayoutManager);
+                break;
+            case R.id.btn_data1:
+                adapter.addData(data1);
+                adapter.notifyDataSetChanged();
+                break;
+            case R.id.btn_data2:
+                adapter.addData(data2);
+                adapter.notifyDataSetChanged();
+                break;
+        }
+    }
+}
